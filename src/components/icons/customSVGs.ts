aee3e42064c1e57bb994c6bb9bdf9ec803461938
Orikson <42471346+Orikson@users.noreply.github.com>
2023-07-11 00:35:59 -0700

    Add custom icons for socials

---
+++ src/components/icons/customSVGs.ts
@@ -0,0 +1,20 @@
+import { h } from "vue";
+import type { IconSet, IconProps } from "vuetify";
+import GithubIcon from "./githubIcon.vue";
+import InstagramIcon from "./instagramIcon.vue";
+import GitlabIcon from "./gitlabIcon.vue";
+import LinkedinIcon from "./linkedinIcon.vue";
+
+const customSvgNameToComponent: any = {
+  GithubIcon,
+  InstagramIcon,
+  GitlabIcon,
+  LinkedinIcon,
+};
+
+const customSVGs: IconSet = {
+  component: (props: IconProps) =>
+    h(customSvgNameToComponent[props.icon as string]),
+};
+
+export { customSVGs /* aliases */ };
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,6 +4,18 @@ import "vuetify/styles";
 
 // Vuetify
 import { createVuetify } from "vuetify";
+import { mdi } from "vuetify/iconsets/mdi";
 
-export default createVuetify();
+// Icons
+import { customSVGs } from "@/components/icons/customSVGs";
+
+export default createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    sets: {
+      mdi,
+      custom: customSVGs,
+    },
+  },
+});
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
