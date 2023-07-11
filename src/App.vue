a53736448e2007d99ccc61ebb3adc617d8856d30
Orikson <42471346+Orikson@users.noreply.github.com>
2023-07-11 00:37:17 -0700

    Add main menu and add gradient background

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <v-app style="overflow: hidden !important">
+  <v-app theme="dark" style="overflow: hidden !important">
     <v-main style="height: 100vh">
-      <BabylonJS v-resize="onResize" style="height: 100%; width: 100%" />
+      <div
+        style="
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          background: radial-gradient(
+            600px at 50% 50%,
+            rgba(29, 78, 216, 0.15),
+            transparent 90%
+          );
+        "
+      >
+        <BabylonJS v-resize="onResize" style="height: 100%; width: 100%" />
+      </div>
+      <MainMenu></MainMenu>
     </v-main>
   </v-app>
 </template>
@@ -9,6 +23,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import BabylonJS from "./components/BabylonJS.vue";
+import MainMenu from "./components/MainMenu.vue";
 import store from "@/store";
 
 export default defineComponent({
@@ -16,6 +31,7 @@ export default defineComponent({
 
   components: {
     BabylonJS,
+    MainMenu,
   },
 
   data() {
@@ -34,3 +50,9 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+body {
+  font-family: "Cascadia Code", sans-serif;
+}
+</style>
