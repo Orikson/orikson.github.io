d6802dd31fdc9ed6f56037946ed7f322d8f4a9ab
Orikson <42471346+Orikson@users.noreply.github.com>
2023-07-14 00:21:25 -0700

    Process commands and pass arguments to them

--- src/ts/terminal/terminal.ts
+++ src/ts/terminal/terminal.ts
@@ -1,15 +1,45 @@
+import { Directories, Folder, Item, isFolder } from "./directories";
+import { Commands } from "./parser/commands";
+
 export class Terminal {
   user: string;
 
-  private currentDirectory: string;
+  private _currentDirectory: string;
+  public get currentDirectory() {
+    return this._currentDirectory;
+  }
+  public set currentDirectory(dir: string) {
+    // reduce dir to absolute path
+    this._currentDirectory = dir;
+  }
+
+  private directories = Directories;
+  private commands = Commands;
 
   constructor(user: string) {
     this.user = user;
-    this.currentDirectory = "/";
+    this._currentDirectory = "/";
   }
 
   get header() {
     return `${this.user}:${this.currentDirectory}$ `;
+  }
+
+  // Processes the command given by text input, and returns the command's output
+  private processCommand(line: string): string {
+    const input = line.split(" ");
+
+    if (input.length == 0) {
+      return "";
+    }
+
+    const command = input[0];
+    const exec = this.commands.get(command);
+    if (!exec) {
+      return `Unrecognized command ${command}. Run the help command to get information on available commands\n`;
+    }
+
+    return exec.callback(this, ...input.slice(1));
   }
 
   // Takes the input command as a string, parses it, processes it, and returns the output as a string
@@ -17,6 +47,34 @@ export class Terminal {
     if (input.length == 0) {
       return "";
     }
-    return `Received input of length ${input.length}: ${input}\n`;
+
+    return this.processCommand(input);
+  }
+
+  // `dir` must be an array where each string is the next directory (i.e. derived from an absolute path)
+  getDirectory(dir: string[]): Folder | null {
+    let curr: Item = this.directories;
+    if (!isFolder(curr)) {
+      return null;
+    }
+
+    for (const directory of dir) {
+      if (directory == "") {
+        continue;
+      }
+
+      if (!isFolder(curr)) {
+        return null;
+      }
+
+      const child = curr.children.get(directory);
+      if (child && isFolder(child)) {
+        curr = child;
+      } else {
+        return null;
+      }
+    }
+
+    return curr as Folder;
   }
 }
