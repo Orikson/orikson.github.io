87daac16b1a5e41324a5902c6ba7a2f087425343
Orikson <42471346+Orikson@users.noreply.github.com>
2023-07-14 07:05:17 -0700

    Add pwd command

--- src/ts/terminal/parser/commands.ts
+++ src/ts/terminal/parser/commands.ts
@@ -1,8 +1,10 @@
 import { Command } from "./commandTypes";
 import { HELP } from "./commands/help";
 import { LS } from "./commands/ls";
+import { PWD } from "./commands/pwd";
 
 export const Commands = new Map<string, Command>([
   ["ls", LS],
   ["help", HELP],
+  ["pwd", PWD],
 ]);
---
+++ src/ts/terminal/parser/commands/pwd.ts
@@ -0,0 +1,17 @@
+import { Terminal } from "../../terminal";
+import { Command } from "../commandTypes";
+
+export const PWD = {
+  command: "pwd",
+  description: "pwd - print the name of the current directory",
+  usage: `Print the full filename of the current working directory.
+pwd
+`,
+  callback: (terminal: Terminal, ...args: string[]) => {
+    if (args.length > 0) {
+      return "Invalid number of arguments. Usage: help [command]\n";
+    }
+
+    return terminal.currentDirectory + "\n";
+  },
+} as Command;
