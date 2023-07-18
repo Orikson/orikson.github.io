1cde61fab7702dc03a0bbe2aa13d0bb59b7430de
Orikson <42471346+Orikson@users.noreply.github.com>
2023-07-18 00:26:32 -0700

    Parse file path

--- src/ts/terminal/parser/parseInputs.ts
+++ src/ts/terminal/parser/parseInputs.ts
@@ -7,6 +7,14 @@ const DirectoryFail = [
   false,
 ] as [DirectoryType, boolean];
 
+const FileFail = [
+  {
+    absolutePath: "",
+    fileName: "",
+  },
+  false,
+] as [FileType, boolean];
+
 /**
  * Parses an input to check if it is in the correct directory format,
  *  and reduces the path to an absolute path
@@ -59,11 +67,54 @@ export function parseDirectory(
   ];
 }
 
-export function parseFile(word: string): FileType {
-  return {
-    absolutePath: "",
-    fileName: "",
-  };
+export function parseFile(
+  word: string,
+  curDirectory: string
+): [FileType, boolean] {
+  if (word.length == 0) {
+    return FileFail;
+  }
+
+  const isRelative = word[0] != "/";
+
+  if (isRelative) {
+    word = curDirectory + "/" + word;
+  }
+  const directories = word.split(/[\\/]/);
+  const fileName = directories.pop();
+  if (!fileName) {
+    return FileFail;
+  }
+
+  const stack: string[] = [];
+  for (const dir of directories) {
+    if (dir == "" || dir == ".") {
+      continue;
+    } else if (dir == "..") {
+      const res = stack.pop();
+      if (res == undefined) {
+        return FileFail;
+      }
+    } else {
+      stack.push(dir);
+    }
+  }
+
+  let path = "";
+  for (const dir of stack) {
+    path += "/" + dir;
+  }
+  if (path == "") {
+    path = "/";
+  }
+
+  return [
+    {
+      absolutePath: path,
+      fileName: fileName,
+    },
+    true,
+  ];
 }
 
 export function parseNumber(word: string): number {
