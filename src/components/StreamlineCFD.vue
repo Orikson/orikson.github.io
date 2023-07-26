9389246599a87ed1bac79e5752771cea4a4b668b
Orikson <42471346+Orikson@users.noreply.github.com>
2023-07-26 00:03:44 -0700

    Update Streamline CFD page

--- src/components/StreamlineCFD.vue
+++ src/components/StreamlineCFD.vue
@@ -7,13 +7,24 @@
     style="margin: 20px"
   >
     <p class="p">
-      Streamline is a computational fluid dynamics (CFD) software package that
-      is built in C++ and OpenCL. Designed for computer graphics applications,
-      it is capable of simulating smoke, fire, and other fluid phenomena in
-      realtime.
+      Streamline is a computational fluid dynamics (CFD) software application
+      that is built in C++ and OpenCL, and uses a UI created in Qt5. Intended
+      for computer graphics applications, it implements various SIGGRAPH fluid
+      dynamics papers from over the years.
     </p>
     <div style="width: 100%; text-align: center">
-      <img style="width: 60%" src="./collisionGif.gif" />
+      <figure>
+        <img style="width: 60%" src="./collisionGif.gif" />
+        <figcaption>
+          Implementation of
+          <a
+            href="http://www.cs.columbia.edu/cg/surfaceliquids/droplets.pdf"
+            target="_blank"
+          >
+            <span class="y">Da et. al, 2016</span>
+          </a>
+        </figcaption>
+      </figure>
     </div>
   </v-card>
 </template>
